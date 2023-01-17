df2366179d0b4314257ca4d193c0fe2bcaf2ce30
danilprofiru <danilprofiru@gmail.com>
2023-01-17 19:24:35 +0300

    zadanie

--- 5.2.py
+++ 5.2.py
@@ -1,1 +1,14 @@
-privet
+for x in range(1, 100000):
+    n = x
+    s = ""
+    while n > 0:
+        s += str(n % 3)
+        n //= 3
+    s = str(x % 3) + s
+    r = 0
+    for i in range(len(s)):
+        r += int(s[i]) * 3 ** i
+        c = str(r)
+    if len(c) >= 4:
+        print(c)
+        break
